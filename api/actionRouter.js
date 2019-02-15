41a0a7d963d8fc6b3afad5f17b25529d18f2d5e0
Nate Boyette <nateboyette@icloud.com>
2019-02-15 08:58:53 -0800

    Add get endpoints for actions and projects.

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -1,12 +1,57 @@
 const express = require('express')
 
-const db = require('../data/helpers/projectModel')
+const db = require('../data/helpers/actionModel')
 //Install/use cors for react
 
 const router = express.Router();
 
 
 // GET
+// GET ALL
+router.get('/', (req, res) => {
+  db.get()
+    .then(actions => {
+      console.log(actions)
+      res.status(200).json({
+        success: true,
+        actions
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        error: "The actions could not be retrieved"
+      })
+    })
+})
+
+
+// GET ACTION BY ID
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+
+  db.get(id)
+    .then(actions => {
+      if (!actions) {
+        return res.status(404).json({
+          error: "The action with the specified ID does not exist"
+        })
+      } else {
+        console.log(actions)
+        res.status(200).json({
+          success: true,
+          actions
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        error: "The projects could not be retrieved"
+      })
+    })
+})
 
 // POST
 
--- api/projectRouter.js
+++ api/projectRouter.js
@@ -8,6 +8,57 @@ const router = express.Router();
 
 // GET
 
+// GET ALL
+router.get('/', (req, res) => {
+  console.log(res.data)
+  db.get()
+  .then(projects => {
+    console.log(projects)
+    res.status(200).json({
+      success: true,
+      projects
+    })
+  })
+  .catch(err => {
+    res.status(500).json({
+      success: false,
+      error: "The projects could not be retrieved"
+    })
+  })
+})
+
+
+// GET PROJECT BY ID
+
+router.get('/:id', (req, res) => {
+const id = req.params.id
+  
+  db.get(id)
+    .then(projects => {
+      
+      if (!projects) {
+        return res.status(404).json({
+          error: "The project with the specified ID does not exist"
+        })
+      } else {
+        console.log(projects.actions)
+        res.status(200).json({
+          success: true,
+          projects
+        })
+      }    
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        success: false,
+        error: "The projects could not be retrieved"
+      })
+    })
+})
+
+
+
 // POST
 
 // PUT
