c1ba4771481190edb27bf46782d1941a1ad414cd
Nate Boyette <nateboyette@icloud.com>
2019-02-15 08:27:36 -0800

    Setup server and express routers.

---
+++ api/actionRouter.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const db = require('../data/helpers/projectModel')
+//Install/use cors for react
+
+const router = express.Router();
+
+
+// GET
+
+// POST
+
+// PUT
+
+// DELETE
+
+
+
+
+
+
+module.exports = router;
